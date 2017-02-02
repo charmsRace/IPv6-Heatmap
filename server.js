038944c7367e3264aa03d3b0d4d12c8a492fab01
Calamitizer <cindersquall@gmail.com>
2017-02-02 17:47:52 -0500

    Add a border to the map, and chose some fonts

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 (function() {
     'use strict';
-    
+
     require('dotenv').config();
-    
+
     var express = require('express');
     var path = require('path');
     var fs = require('fs');
@@ -12,13 +12,13 @@
     var ejs = require('ejs');
     var mongoose = require('mongoose');
     var http = require('http');
-    
+
     var db = require('./server/methods/index.js')
-    
+
     var port = process.env.PORT || 3000;
-    
+
     var iphm = express();
-    
+
     // view engine setup
     iphm.set('views', path.join(__dirname, 'views'));
     iphm.engine('html', ejs.renderFile);
@@ -27,41 +27,41 @@
     iphm.use(bodyParser.json());
     iphm.use(bodyParser.urlencoded({extended: true}));
     iphm.use(cookieParser());
-    
+
     var loggingRouter = require('./server/routers/logging.js');
     var rawRouter = require('./server/routers/raw.js');
     var apiRouter = require('./server/routers/api.js');
     var siteRouter = require('./server/routers/site.js');
     var staticRouter = require('./server/routers/static.js');
     var fallbackRouter = require('./server/routers/fallback.js');
-    
+
     iphm.use(loggingRouter);
     iphm.use('/raw', rawRouter);
     iphm.use('/api/v0.1', apiRouter);
     iphm.use('/', siteRouter);
     iphm.use('/', staticRouter);
     iphm.use(fallbackRouter);
-    
+
     iphm.set('port', port);
     var server = http.createServer(iphm);
-    
+
     console.log('start');
-    
+
     var listen = function() {
         iphm.listen(iphm.get('port'), function() {
             console.log('Express server listening on port ' + iphm.get('port'));
         });
     };
-    
+
     listen();
-    
+
     db
         .connect()
         .once('open', function() {
             console.log('Online!');
             //db.populate()
         });
-    
+
     /*
     var connect = function() {
         var options = {};
@@ -69,7 +69,7 @@
             .connect(dburi, options)
             .connection;
     };
-    
+
     connect()
         .on('error', console.log)
         .on('disconnected', connect)
